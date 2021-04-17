a885c96bb7f2d0390465b48aa282bf6e3a8f9d2f
Sean Millane <jmillaneiv@gmail.com>
2021-04-17 16:28:07 -0400

    homepage styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,11 +23,21 @@ const useStyles = makeStyles( {
     cardStyling: {
         alignItems: 'center',
         backgroundColor: 'rgba(255,255,255,0.1)',
+    },
+    mobileCardStyling: {
+        alignItems: 'center',
+        backgroundColor: 'rgba(255,255,255,0.1)',
+        width: '100vh',
     }
 });
 
 export default function Home() {
     const classes = useStyles();
+    const mobileFirstName = <Typography variant='h3' align='center' gutterBottom>Sean</Typography>;
+    const mobileLastName = <Typography variant='h3' align='center' gutterBottom>Millane</Typography>;
+    const NamePlacement = <Typography variant='h3' align='center' gutterBottom>Sean Millane</Typography>;
+    const deviceNamePlacement = window.innerWidth >= 650 ? NamePlacement : mobileFirstName && mobileLastName;
+    const deviceCardStyling = window.innerWidth >= 650 ? classes.cardStyling : classes.mobileCardStyling;
     return (
         <main className = {classes.root}>
             <Grid
@@ -35,12 +45,12 @@ export default function Home() {
             spacing = {0}
             alignItems = 'center'
             justify = 'center'
-            style={{ minHeight: '35vh' }}
+            style={{ height: '35vh' }}
             >
                 <Grid item xs={8} sm={6} md={4} lg={5} xl={3} >
-                    <Card className = {classes.cardStyling} variant='elevation' elevation='10'>
+                    <Card className={deviceCardStyling} variant='elevation' elevation='10'>
                         <CardContent className = {classes.textStyle}>
-                            <Typography variant='h3' align='center' gutterBottom>Sean Millane</Typography>
+                            {deviceNamePlacement}
                             <Typography variant='h5' align='center' gutterBottom>Highly motivated, self-taught software engineer with a passion for software development and technology.</Typography>
                             <Typography variant='h5' align='center' gutterBottom>Detail-oriented civil engineer looking to transition to software engineering.</Typography>
                         </CardContent>
