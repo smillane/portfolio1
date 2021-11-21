c7e7e87f85ddb0480365bd4b6edf6d6d1fab08be
Sean Millane <60592728+smillane@users.noreply.github.com>
2021-11-21 00:44:12 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import clsx from 'clsx';
-import { NavLink } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, IconButton, ListItem, ListItemText, CssBaseline } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, CssBaseline } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
